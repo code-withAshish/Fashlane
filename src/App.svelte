9d55d3f4487a5f45c118117ea3daa57cb2ac40f5
Ashish Bhushan Kumar <abhushan064@gmail.com>
2022-10-14 15:42:44 +0530

    fix: Responsive design

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,6 @@
   import DashBoard from "./lib/DashBoard.svelte";
 </script>
 
-<main class="flex items-center h-screen justify-center md:p-10 text-white">
+<main class="flex items-center h-screen justify-center  md:p-10 text-white">
   <DashBoard />
 </main>
